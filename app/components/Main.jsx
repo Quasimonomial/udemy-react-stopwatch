6f3dd305b2207c9b69e00faf23703171598ca71a
Travis Herrick <quasimonomial@gmail.com>
2018-06-26 14:18:56 -0700

    add Navbar to project

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 
+import Nav from 'Nav'
+
 var Main = (props) => {
   return (
     <div>
+      <Nav />
       <Switch>
       </Switch>
     </div>
---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Tested Stopwatch</li>
+            <li>
+              <NavLink to="/" activeClassName='active-link' exact>Timer</NavLink>
+            </li>
+            <li>
+              <NavLink to="/" activeClassName='active-link' exact>Countdown</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+              <li className="menu-text">Created by <a href="https://github.com/Quasimonomial/" target="_blank">Travis</a></li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Nav;
